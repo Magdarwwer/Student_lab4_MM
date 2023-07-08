df2a9797138215f5ed94c356313190f6492383af
Magda <Magda@LAPTOP-K7D4V89L>
2023-07-08 13:53:37 +0200

    Dodaj pliki projektów.

---
+++ Student_lab4/StudentContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.Text;
+using Student_lab4;
+using Student_lab4.Configuration;
+using Student_lab4.Controllers;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Student_lab4
+{
+    public class StudentContext : IdentityDbContext<UserEntity,
+UserRole, int>
+    {
+        public StudentContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; } = null!;
+    }
+
+}
---
+++ Student_lab4/UserEntity.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Student_lab4
+{
+    public class UserEntity : IdentityUser<int>
+    {
+    }
+}
